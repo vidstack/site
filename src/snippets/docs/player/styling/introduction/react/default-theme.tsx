47a080f5ebb5f64af2280f789bf96d71864054c2
Rahim Alwer <rahim.alwer@gmail.com>
2023-10-30 16:03:44 +1100

    add styling intro page

---
+++ src/snippets/docs/player/styling/introduction/react/default-theme.tsx
@@ -0,0 +1,11 @@
+<Poster className="vds-poster" />
+
+<PlayButton className="vds-button">{/* ... */}</PlayButton>
+
+<Slider.Root className="vds-slider">
+  {/* ... */}
+</Slider.Root>
+
+<Menu.Root className="vds-menu">
+  {/* ... */}
+</Menu.Root>
---
+++ src/snippets/docs/player/styling/introduction/react/styled-components.tsx
@@ -0,0 +1,14 @@
+// Apply styles via classes.
+
+<Poster className="poster" />
+
+<PlayButton className="play-button">{/* ... */}</PlayButton>
+
+<Slider.Root className="slider">
+  <Slider.TrackFill className="slider-track-fill" />
+  {/* ... */}
+</Slider.Root>
+
+<Menu.Root className="menu">
+  {/* ... */}
+</Menu.Root>
