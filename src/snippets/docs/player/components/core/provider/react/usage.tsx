b46ebc9f062bb07c895e539b623aa2197dee6420
Rahim Alwer <rahim.alwer@gmail.com>
2023-11-08 16:48:55 +1100

    add simple docs to provider page

---
+++ src/snippets/docs/player/components/core/provider/react/usage.tsx
@@ -0,0 +1,8 @@
+<MediaPlayer>
+  {/* Can detach from DOM and move around to create floating, popup, and mini players. */}
+  <MediaProvider>
+    {/* Content here that should be rendered inside outlet (e.g., poster). */}
+  </MediaProvider>
+
+  {/* Controls and other media UI can be outside and placed on top. */}
+</MediaPlayer>;
