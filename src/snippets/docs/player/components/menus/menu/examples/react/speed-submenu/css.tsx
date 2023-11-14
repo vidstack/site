86161ffee39516c0789f0e736de9201f6082ba04
Rahim Alwer <rahim.alwer@gmail.com>
2023-11-14 14:39:06 +1100

    add speed submenu examples

---
+++ src/snippets/docs/player/components/menus/menu/examples/react/speed-submenu/css.tsx
@@ -0,0 +1,44 @@
+import { type ReactElement } from 'react';
+
+import { Menu, usePlaybackRateOptions } from '@vidstack/react';
+// See "Icons" component page for setup before importing the following:
+import { ChevronLeftIcon, ChevronRightIcon, OdometerIcon } from '@vidstack/react/icons';
+
+function SpeedSubmenu() {
+  const options = usePlaybackRateOptions(),
+    hint = options.selectedValue === '1' ? 'Normal' : options.selectedValue + 'x';
+  return (
+    <Menu.Root>
+      <SubmenuButton label="Speed" hint={hint} disabled={options.disabled} icon={OdometerIcon} />
+      <Menu.Content className="media-submenu">
+        <Menu.RadioGroup className="media-radio-group" value={options.selectedValue}>
+          {options.map(({ label, value, select }) => (
+            <Menu.Radio className="media-radio" value={value} onSelect={select} key={value}>
+              <div className="media-radio-check" />
+              <span className="media-radio-label">{label}</span>
+            </Menu.Radio>
+          ))}
+        </Menu.RadioGroup>
+      </Menu.Content>
+    </Menu.Root>
+  );
+}
+
+interface SubmenuButtonProps {
+  label: string;
+  hint: string;
+  disabled?: boolean;
+  icon: ReactElement;
+}
+
+function SubmenuButton({ label, hint, icon: Icon, disabled }: SubmenuButtonProps) {
+  return (
+    <Menu.Button className="media-submenu-button" disabled={disabled}>
+      <ChevronLeftIcon className="media-submenu-close-icon" />
+      <Icon className="media-submenu-icon" />
+      <span className="media-submenu-label">{label}</span>
+      <span className="media-submenu-hint">{hint}</span>
+      <ChevronRightIcon className="media-submenu-open-icon" />
+    </Menu.Button>
+  );
+}
