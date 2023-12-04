0016009dde1e5ea32bd26d935f4e2f0a582dfb5f
Rahim Alwer <rahim.alwer@gmail.com>
2023-12-04 12:34:03 +1100

    document tailwind selector plugin option

--- src/snippets/docs/player/styling/tailwind/react/options.js
+++ src/snippets/docs/player/styling/tailwind/react/options.js
@@ -1,4 +1,6 @@
 require('@vidstack/react/tailwind.cjs')({
+  // Optimize output by specifying player selector.
+  selector: '.media-player',
   // Change the media variants prefix.
   prefix: 'media',
 });
--- src/snippets/docs/player/styling/tailwind/wc/options.js
+++ src/snippets/docs/player/styling/tailwind/wc/options.js
@@ -1,4 +1,6 @@
 require('vidstack/tailwind.cjs')({
+  // Optimize output by specifying player selector.
+  selector: '.media-player',
   // Change the media variants prefix.
   prefix: 'media',
   // Enables more efficient selectors.
