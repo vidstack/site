632cbd52af53160fdd3af9c38fdb023979b0e81d
Rahim Alwer <rahim.alwer@gmail.com>
2024-01-30 18:37:25 +1100

    include plyr theme in vidstack bundle

--- src/layouts/vidstack-lib.svelte
+++ src/layouts/vidstack-lib.svelte
@@ -3,6 +3,7 @@
   import 'vidstack/player/styles/default/theme.css';
   import 'vidstack/player/styles/default/layouts/audio.css';
   import 'vidstack/player/styles/default/layouts/video.css';
+  import 'vidstack/player/styles/plyr/theme.css';
   // Elements
   import 'vidstack/player';
   import 'vidstack/player/layouts';
