cbc183e77e257dbf094dbc67aceb6ee71c30e717
rgehbt <74761884+Gehbt@users.noreply.github.com>
2024-02-29 11:21:40 +0800

    fix compiler options nuxt config (#25)

--- src/snippets/docs/player/getting-started/installation/vue/config/nuxt.ts
+++ src/snippets/docs/player/getting-started/installation/vue/config/nuxt.ts
@@ -1,10 +1,8 @@
 export default defineNuxtConfig({
   vue: {
     // @hl-start
-    template: {
-      compilerOptions: {
-        isCustomElement: (tag) => tag.startsWith('media-'),
-      },
+    compilerOptions: {
+      isCustomElement: (tag) => tag.startsWith('media-'),
     },
     // @hl-end
   },
