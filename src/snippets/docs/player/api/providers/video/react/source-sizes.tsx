2325e63e1378696898d8f4636bd3a89910980937
Rahim Alwer <rahim.alwer@gmail.com>
2024-03-21 15:15:57 +1100

    add source sizes

---
+++ src/snippets/docs/player/api/providers/video/react/source-sizes.tsx
@@ -0,0 +1,22 @@
+<MediaPlayer
+  src={[
+    {
+      src: 'https://media-files.vidstack.io/1080p.mp4',
+      type: 'video/mp4',
+      width: 1920,
+      height: 1080,
+    },
+    {
+      src: 'https://media-files.vidstack.io/720p.mp4',
+      type: 'video/mp4',
+      width: 1280,
+      height: 720,
+    },
+    {
+      src: 'https://media-files.vidstack.io/480p.mp4',
+      type: 'video/mp4',
+      width: 853,
+      height: 480,
+    },
+  ]}
+/>;
