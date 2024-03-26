4e891c11fb2211522c757b749586ee4aac515c1a
Rahim Alwer <rahim.alwer@gmail.com>
2024-03-26 13:55:25 +1100

    add svelte hydration bug warning to install page

---
+++ src/docs/installation/conditions/if-ssr.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { selections } from '../selection/selection-stores';
+
+  const { bundler } = selections;
+</script>
+
+{#if $bundler === 'astro' || $bundler === 'nuxt' || $bundler === 'sveltekit' || $bundler === 'solidstart'}
+  <slot />
+{/if}
---
+++ src/snippets/docs/player/getting-started/installation/svelte/ssr.svelte
@@ -0,0 +1,3 @@
+{#await import('./Player.svelte') then { default: Player }}
+  <svelte:component this={Player} />
+{/await}
