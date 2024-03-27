df13bd9f0f8dcb870fced24aae8caca36f4698b3
Rahim Alwer <rahim.alwer@gmail.com>
2024-03-27 20:32:57 +1100

    add default layout color schemes

---
+++ src/snippets/docs/player/components/layouts/default-layout/react/color-scheme.tsx
@@ -0,0 +1,6 @@
+// Light
+<DefaultAudioLayout colorScheme="light" />
+<DefaultVideoLayout colorScheme="light" />
+// Dark
+<DefaultAudioLayout colorScheme="dark" />
+<DefaultVideoLayout colorScheme="dark" />
---
+++ src/snippets/docs/player/components/layouts/default-layout/react/parent-color-scheme.tsx
@@ -0,0 +1,9 @@
+// Set `light` or `dark` on a parent element.
+<html className="light">
+  {/* ... */}
+  <MediaPlayer>
+    {/* ... */}
+    <DefaultAudioLayout />
+    <DefaultVideoLayout />
+  </MediaPlayer>
+</html>;
