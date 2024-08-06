ddc1ecf156822e3b8037a01977a8ba78f6aa7c98
Rahim Alwer <rahim.alwer@gmail.com>
2024-08-06 21:08:48 +1000

    add note to poster next example

--- src/snippets/docs/player/components/display/poster/react/next.tsx
+++ src/snippets/docs/player/components/display/poster/react/next.tsx
@@ -4,6 +4,7 @@ import { Poster } from '@vidstack/react';
 
 import poster from './poster.webp';
 
+// See styling examples above.
 <Poster asChild>
   <Image src={poster} width={1920} height={1080} alt="A description of my image." />
 </Poster>;
